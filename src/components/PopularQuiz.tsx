6db36cb5815b6993ccea192ec963eb7d1ecaa175
Caroline Maia Abreu da Silva <carolmas99@gmail.com>
2023-02-10 11:50:03 -0300

    feat: Link from react router dom added

--- src/components/PopularQuiz.tsx
+++ src/components/PopularQuiz.tsx
@@ -2,53 +2,60 @@ import computer from '../assets/computer.png'
 import star from '../assets/star.svg'
 import sport from '../assets/sport.png'
 import music from '../assets/music.png'
+import { Link } from 'react-router-dom'
 import '../styles/popularQuiz.scss'
 
 export function PopularQuiz(){
   return(
     <div className="popular-quiz">
       <h3>Popular Quiz</h3>
-      <div className="quiz">
-        <div className='technology'>
-          <img src={computer} alt="" />
+      <Link to="/detail">
+        <div className="quiz">
+          <div className='technology'>
+            <img src={computer} alt="" />
+          </div>
+          <div className='quiz-info'>
+            <h3>Technology</h3>
+            <p>10 Questions</p>
+          </div>
+          <div className="score">
+            <img src={star} alt="" />
+            <p>4.8</p>
+          </div>
         </div>
-        <div className='quiz-info'>
-          <h3>Technology</h3>
-          <p>10 Questions</p>
-        </div>
-        <div className="score">
-          <img src={star} alt="" />
-          <p>4.8</p>
-        </div>
-      </div>
+      </Link>
 
-      <div className="quiz">
-        <div className='sport'>
-          <img src={sport} alt="" />
+      <Link to="/detail-sport">
+        <div className="quiz">
+          <div className='sport'>
+            <img src={sport} alt="" />
+          </div>
+          <div className='quiz-info'>
+            <h3>Sport</h3>
+            <p>10 Questions</p>
+          </div>
+          <div className="score">
+            <img src={star} alt="" />
+            <p>4.8</p>
+          </div>
         </div>
-        <div className='quiz-info'>
-          <h3>Sport</h3>
-          <p>10 Questions</p>
-        </div>
-        <div className="score">
-          <img src={star} alt="" />
-          <p>4.8</p>
-        </div>
-      </div>
+      </Link>
 
-      <div className="quiz">
-        <div className='music'>
-          <img src={music} alt="" />
+      <Link to="/detail">
+        <div className="quiz">
+          <div className='music'>
+            <img src={music} alt="" />
+          </div>
+          <div className='quiz-info'>
+            <h3>Music</h3>
+            <p>10 Questions</p>
+          </div>
+          <div className="score">
+            <img src={star} alt="" />
+            <p>4.8</p>
+          </div>
         </div>
-        <div className='quiz-info'>
-          <h3>Music</h3>
-          <p>10 Questions</p>
-        </div>
-        <div className="score">
-          <img src={star} alt="" />
-          <p>4.8</p>
-        </div>
-      </div>
+      </Link>
     </div>
   )
 }
