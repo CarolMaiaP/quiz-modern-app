612a46f1c63be4f111a2e2810b45a784dd595d1a
Caroline Maia Abreu da Silva <carolmas99@gmail.com>
2023-02-10 11:47:46 -0300

    feat: React-router-dom added

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,18 @@
-import { Header } from "./components/Header"
-import { PopularQuiz } from "./components/PopularQuiz"
-import { RecentQuiz } from "./components/RecentQuiz"
-import { Result } from "./components/Result"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import { DetailSportQuiz } from "./pages/DetailSportQuiz"
+import { Home } from "./pages/Home"
 import './styles/global.scss'
 
 function App() {
 
   return (
     <div className="App">
-      <Header />
-      <Result />
-      <RecentQuiz />
-      <PopularQuiz />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/detail-sport" element={<DetailSportQuiz />} />
+        </Routes>
+      </Router>
     </div>
   )
 }
