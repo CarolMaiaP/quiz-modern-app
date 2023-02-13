b730e4853bff0477a5557d1f030d9ca0fcfd2980
Caroline Maia Abreu da Silva <carolmas99@gmail.com>
2023-02-13 18:12:40 -0300

    feat: Question functionalities added

--- src/components/Question.tsx
+++ src/components/Question.tsx
@@ -1,23 +1,38 @@
+import { useContext, useState } from 'react'
+import { QuizContext } from '../context/QuizContext'
 import '../styles/question.scss'
 import { OptionQuiz } from './OptionsQuiz'
+import { QuizOver } from './QuizOver';
 
 export function Question(){
+  const [quizState, dispatch]:any = useContext(QuizContext);
+  const currentQuestion = quizState.questions[quizState.currentQuestion]
+
+
   return(
-    <div className="question">
-      <h3>The image below is a field image 
-        used for?</h3>
+    <>
+      {!currentQuestion ?
+        <QuizOver />
+      :
+        <div className="question">
+          <p>{quizState.currentQuestion + 1} de {quizState.questions.length}</p>
+          <h3>{currentQuestion.question}</h3>
 
-      <OptionQuiz />
+          <OptionQuiz />
 
-      <div className="actions">
-        <div className="previous">
-          <button>Previous</button>
-        </div>
+          <div className="actions">
+            <div className="previous">
+              <button>Previous</button>
+            </div>
 
-        <div className="next">
-          <button>Next</button>
-        </div>
-      </div>
-    </div>
+            <div className="next">
+              <button onClick={() => dispatch({ type: "NEXT_QUESTION" })}>Next</button>
+            </div>
+          </div>
+        </div> 
+      }
+    </>
+
+    
   )
 }
