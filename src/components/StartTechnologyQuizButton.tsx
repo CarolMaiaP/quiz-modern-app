f47b89e7a73a7088ee79366f0e3f5f045cb5cc3d
Caroline Maia Abreu da Silva <carolmas99@gmail.com>
2023-02-13 18:07:37 -0300

    feat: Change state to playing

--- src/components/StartTechnologyQuizButton.tsx
+++ src/components/StartTechnologyQuizButton.tsx
@@ -1,11 +1,15 @@
 import '../styles/startQuizButton.scss'
 import { Link } from 'react-router-dom'
+import { useContext } from 'react';
+import { QuizContext } from '../context/QuizContext';
 
 export function StartTechnologyQuizButton(){
+  const [quizState, dispatch]:any = useContext(QuizContext);
+
   return(
     <div className="start-button">
       <Link to="/technology-quiz">
-        <button>Get Started</button>
+        <button onClick={() =>  dispatch({type: "CHANGE_STATE"})}>Get Started</button>
       </Link>
     </div>
   )
