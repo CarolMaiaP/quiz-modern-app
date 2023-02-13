dcfbd8dbd1a5f8abb5d0883b5a6d856f623b70fc
Caroline Maia Abreu da Silva <carolmas99@gmail.com>
2023-02-13 18:06:00 -0300

    feat: Technology questions data added

---
+++ src/data/techQuestion.ts
@@ -0,0 +1,32 @@
+const data = [
+  {
+    question: "O que significa a sigla “www” na internet?",
+    options: [
+      "World wide web",
+      "Web world wide",
+      "Web wide World",
+    ],
+    answer: "World wide web",
+  },
+  {
+    question: "Qual foi a primeira rede social da história da internet?",
+    options: [
+      "Classmate",
+      "MySpace", 
+      "Orkut",
+    ],
+    answer: "Classmate",
+  },
+  {
+    question: "Quando foi criado o primeiro smartphone da história?",
+    options: ["1994", "2000", "1998"],
+    answer: "1994",
+  },
+  {
+    question: "Quantos bits cabem em um byte?",
+    options: ["8 bits", "1 bit", "12 bits"],
+    answer: "8 bits",
+  },
+];
+
+export default data;
