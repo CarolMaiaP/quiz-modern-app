c94068a0df13ef99caab97efe96bc5fa696bfc3e
Caroline Maia Abreu da Silva <carolmas99@gmail.com>
2023-02-14 14:44:32 -0300

    feat: Technology options quiz added

--- src/components/OptionsQuiz.tsx
+++ src/components/OptionsQuiz.tsx
@@ -1,24 +1,37 @@
+import { useContext } from 'react';
+import { QuizContext } from '../context/TechContext';
 import '../styles/options.scss'
 
-export function OptionQuiz(){
+export function OptionsQuiz(){
+  const [quizState, dispatch]:any = useContext(QuizContext);
+  const currentQuestion = quizState.questions[quizState.currentQuestion]
+  const answer = currentQuestion.answer
+
+  console.log(quizState.score)
+
+  function handleSelectOption(option:any){
+    dispatch({
+      type: "CHECK_ANSWER",
+      payload: {answer: currentQuestion.answer, option}
+    })
+  }
+
   return(
     <div className="options">
-      <div className="option">
-        <input className='radio' type="radio" id="football" name='option' />
-        <label htmlFor="football">Football</label>
-      </div>
-      <div className="option">
-        <input className='radio' type="radio" id="basketball" name='option' />
-        <label htmlFor="basketball">Basketball</label>
-      </div>
-      <div className="option">
-        <input className='radio' type="radio" id="volleyball" name='option' />
-        <label htmlFor="volleyball">Volleyball</label>
-      </div>
-      <div className="option">
-        <input className='radio' type="radio" id="shuttlecock" name='option' />
-        <label htmlFor="shuttlecock">Shuttlecock</label>
-      </div>
+      {currentQuestion.options.map((option:any) => {
+        return(
+        <div className={
+          `option 
+            ${quizState.answerSelected && option === answer ? "correct" : ""}
+            ${quizState.answerSelected && option !== answer ? "wrong" : ""}
+            ${quizState.answerSelected ? "blocked" : ""}`
+          } 
+          key={option}>
+          <input className='radio' type="radio" id={option} name='option' disabled={!quizState.answerSelected} />
+          <label htmlFor={option} onClick={() => handleSelectOption(option)}>{option}</label>
+        </div>
+        )
+      })}
     </div>
   )
 }
