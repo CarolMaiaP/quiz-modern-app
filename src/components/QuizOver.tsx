df965c05a9177ecf4ded422fddb10786dc3c7972
Caroline Maia Abreu da Silva <carolmas99@gmail.com>
2023-02-14 14:41:58 -0300

    feat: Quiz over score added

--- src/components/QuizOver.tsx
+++ src/components/QuizOver.tsx
@@ -1,7 +1,14 @@
+import { useContext } from "react";
+import { QuizContext } from "../context/TechContext";
+import '../styles/quizOver.scss'
+
 export function QuizOver(){
+  const [quizState, dispatch]:any = useContext(QuizContext);
+
   return(
     <div className="quiz-over">
       <h1>Quiz over</h1>
+      <h4>Pontuação: {quizState.score} / {quizState.questions.length}</h4>
     </div>
   )
 }
