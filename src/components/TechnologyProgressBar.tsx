1283038e9ae01e30e0a3f60dbb00ba6c38df5899
Caroline Maia Abreu da Silva <carolmas99@gmail.com>
2023-02-14 14:45:42 -0300

    feat: Functional progressbar added

--- src/components/TechnologyProgressBar.tsx
+++ src/components/TechnologyProgressBar.tsx
@@ -1,14 +1,23 @@
+import { useContext } from 'react';
+import { QuizContext } from '../context/TechContext';
 import '../styles/tecnologyProgressBar.scss'
 
 export function TechnologyProgressBar(){
+  const [quizState, dispatch]:any = useContext(QuizContext);
+  const currentQuestion = quizState.currentQuestion
+  const totalQuestions = quizState.questions.length
+  const completedPercentage = currentQuestion > 0 ? Math.round((currentQuestion / totalQuestions ) * 100) : 0
+
+  console.log("aquiiii", completedPercentage)
+
   return(
     <div className="progress-bar-contain">
       <div className="tecnology-progress-bar">
-        <div className="tecnology-quiz-completed">
+        <div className="tecnology-quiz-completed" style={{width: `${completedPercentage}%`}}>
 
         </div>
       </div>
-      <p>Completed 64%</p>
+      <p>Completed: {completedPercentage}</p>
     </div>
   )
 }
