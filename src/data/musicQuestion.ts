dcc5a78bc3b4937987586007206e2853b45e40e7
Caroline Maia Abreu da Silva <carolmas99@gmail.com>
2023-02-15 14:38:19 -0300

    feat: music questions data added

---
+++ src/data/musicQuestion.ts
@@ -0,0 +1,40 @@
+const sportdata = [
+  {
+    question: "Quais são os membros no Nirvana?",
+    options: [
+      "Kurt Cobain, Krist Novoselic e Dave Grow",
+      "Kurt Cobayn, Krist Novaselic e Dave Grohl",
+      "Kurt Cobain, Krist Novoselic e Dave Grohl",
+    ],
+    answer: "Kurt Cobain, Krist Novoselic e Dave Grohl",
+  },
+  {
+    question: "Em que ano o Guns N' Roses foi criado?",
+    options: [
+      "1990",
+      "1985", 
+      "1983",
+    ],
+    answer: "1985",
+  },
+  {
+    question: "Qual o ano de formação do Aerosmith?",
+    options: [
+      "1975",
+      "1968", 
+      "1970",
+    ],
+    answer: "1970",
+  },
+  {
+    question: "Antes da Legião Urbana qual foi a banda que Renato Russo teve?",
+    options: [
+      "Eletricidade",
+      "Aborto Elétrico", 
+      "Religião Urbana",
+    ],
+    answer: "Aborto Elétrico",
+  },
+];
+
+export default sportdata;
