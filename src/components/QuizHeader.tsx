fe10e11179a1ef4159a6372bc15619157a7671b5
Caroline Maia Abreu da Silva <carolmas99@gmail.com>
2023-02-15 10:33:39 -0300

    feat: Dinamic quiz name added

--- src/components/QuizHeader.tsx
+++ src/components/QuizHeader.tsx
@@ -1,13 +1,17 @@
 import leftArrow from '../assets/left-arrow.svg'
 import { Link } from 'react-router-dom'
-import '../styles/quizTechnologyHeader.scss'
+import '../styles/quizHeader.scss'
 
-export function QuizTechnologyHeader(){
+interface quizHeaderProps{
+  quiz: string;
+}
+
+export function QuizHeader(props:quizHeaderProps){
   return(
     <header>
       <div className="technology-header">
       <Link to="/"><img src={leftArrow} alt="" /></Link>
-        <h4>Technology</h4>
+        <h4>{props.quiz}</h4>
       </div>
       <div className="time">
         
