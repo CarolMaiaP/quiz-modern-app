7d67e590eea856ff17cb7273eec766709f4f5b1a
Caroline Maia Abreu da Silva <carolmas99@gmail.com>
2023-02-15 12:52:12 -0300

    feat: Initialize quiz button dinamic added

--- src/components/StartSportQuizButton.tsx
+++ src/components/StartSportQuizButton.tsx
@@ -6,14 +6,11 @@ import { QuizContext } from '../context/TechContext';
 export function StartSportQuizButton(){
   const [quizState, dispatch]:any = useContext(QuizContext);
 
-  function handleStartSportQuiz(){
-    console.log("setQuestions")
-  }
 
   return(
     <div className="start-button">
       <Link to="/sport-quiz">
-        <button onClick={handleStartSportQuiz}>Get Started</button>
+        <button onClick={() =>  dispatch({type: "SPORT_QUIZ"})}>Get Started</button>
       </Link>
     </div>
   )
--- src/components/StartTechnologyQuizButton.tsx
+++ src/components/StartTechnologyQuizButton.tsx
@@ -1,15 +1,16 @@
 import '../styles/startQuizButton.scss'
 import { Link } from 'react-router-dom'
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { QuizContext } from '../context/TechContext';
 
 export function StartTechnologyQuizButton(){
   const [quizState, dispatch]:any = useContext(QuizContext);
+  const [ quizCategory, setQuizCategory ] = useState('sport' || 'technology')
 
   return(
     <div className="start-button">
       <Link to="/technology-quiz">
-        <button onClick={() =>  dispatch({type: "CHANGE_STATE"})}>Get Started</button>
+        <button onClick={() =>  dispatch({type: "TECH_QUIZ"})}>Get Started</button>
       </Link>
     </div>
   )
